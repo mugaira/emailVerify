07bad3e8ae453bc2dc9bd6a081a56f3f4580c1aa
mugaira <mugairainamdar@gmail.com>
2022-10-07 09:00:01 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import  express  from 'express';
 import dotenv from "dotenv";
+import path from 'path';
 import connectDB from "./config/db.js"
 import routes from './route.js';
 
@@ -9,11 +10,23 @@ connectDB()
 const app = express();
 app.use(express.json()) //body parsing converting json file into javascript readable language.
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
+
 
 app.use('/api',routes)
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+ 
+  app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+ } else {
+  app.get('/', (req, res) => {
+   res.send('API is running');
+  });
+ }
+ 
 
 
 const PORT = process.env.PORT || 5000;
